be5e91acca2f09b2a632230dcb45369d1e91994d
Adrian Victor <adrianvic.dev@outlook.com>
2024-04-04 07:51:38 -0300

    First commit.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Website
-my website, currently hosted on InfinityFree (thanks) at http://adrian.rf.gd/
---
+++ assets/main.js
@@ -0,0 +1,32 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+      <aside>
+      <nav class="box profile">
+          <img class="decorativeImage" src="./assets/logo.jpg" alt="logo - Shuu Maiko from Niskeoi">
+          <h1>Adrian Victor</h1>
+          <p>@tenkuma</p>
+          <p>Studying computing on <a href="http://ifc.edu.br/">IFC</a>, interessed in programming, hacking e and security.</p>
+      </nav> 
+
+      <div id="links" class="centerText box">
+          <p>Links</p>
+          <p><a href="index.html">Home</a> - <a href="http://gallery.adrian.rf.gd/">Gallery</a> - <a href="https://github.com/adrianvic/">GitHub</a></p>
+      </div>
+      
+      <div id="footer" class="box">
+          <footer>
+              <p>2024 - Adrian Victor, under <a href="https://unlicense.org/">Unlicense</a>, on <a href="https://github.com/adrianvic/website/">GitHub</a>.</p> 
+          </footer>
+      </div>
+  </aside>
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
+  
